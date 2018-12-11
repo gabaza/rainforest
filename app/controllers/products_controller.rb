7501ad3689c9ce881766457784efe5470c6e3d39
davepodgorski <davepodgorski@gmail.com>
2018-12-11 12:03:13 -0500

    index working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,19 @@
-class PagesController < ApplicationController
+class ProductsController < ApplicationController
 
 
-def action
+def create
+@product = Product.new
+@product.name = params[:product][:name]
+@product.description = params[:product][:description]
+@product.price_in_cents = params[:product][:price_in_cents]
 end
 
 def index
+  @products = Product.all
 end
 
 def new
+@product = Product.new  
 end
 
 def edit

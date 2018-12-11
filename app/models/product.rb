909bc9036b03c78da5b5efb0a78ecc4d514a7662
Gabriela <gab.jo.cuello@gmail.com>
2018-12-11 16:23:06 -0500

    Create working validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   has_many :reviews
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price_in_cents, presence: true
+  validates :price_in_cents, numericality: true
+  
 end

64063fe7ed3458e46967bcd870e95be7df709fe4
Gabriela <gab.jo.cuello@gmail.com>
2018-12-11 17:02:34 -0500

    Complete new product form with working submission button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,8 +6,11 @@ def create
   @product.name = params[:product][:name]
   @product.description = params[:product][:description]
   @product.price_in_cents = params[:product][:price_in_cents]
-
-
+  if @product.save
+    redirect_to product_path(@product)
+  else
+    render :new
+  end
 end
 
 def index
@@ -16,6 +19,7 @@ end
 
 def new
   @product = Product.new
+
 end
 
 def edit

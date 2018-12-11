deb8cd5616cb73de21d5a242b03a4f04215f73a8
Gabriela <gab.jo.cuello@gmail.com>
2018-12-11 14:09:37 -0500

    Create buttons for edit and review

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,10 @@ def create
 @product.name = params[:product][:name]
 @product.description = params[:product][:description]
 @product.price_in_cents = params[:product][:price_in_cents]
+
+@review = Review.new
+@review.name = params[:product][:name]
+@review.text
 end
 
 def index
@@ -14,21 +18,24 @@ end
 
 def new
 @product = Product.new
+@review = Review.new
 end
 
 def edit
   @product = Product.find(params[:id])
+  @review = Review.find(params[:id])
 end
 
 def show
   @product = Product.find(params[:id])
-  # @review = @picture.review
+  @reviews = @product.reviews
 end
 
 def update
 end
 
 def delete
+  @review = Review.delete(params[:id])
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_152358) do
+ActiveRecord::Schema.define(version: 2018_12_11_183113) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2018_12_11_152358) do
     t.integer "price_in_cents"
     t.datetime "updated_at", null: false
     t.datetime "created_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "name"
+    t.text "text"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

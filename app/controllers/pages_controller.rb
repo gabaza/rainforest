8f36b6139d4547e47747171fd5443597549b968b
Gabriela <gab.jo.cuello@gmail.com>
2018-12-11 10:43:12 -0500

    Define restful methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,27 @@
 class PagesController < ApplicationController
+
+
+def action
+end
+
+def index
+end
+
+def new
+end
+
+def edit
+end
+
+def show
+end
+
+def update
+end
+
+def delete
+end
+
+
+
 end
